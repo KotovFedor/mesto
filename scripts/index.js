be9355f3b2174c90832fa7f9d449338db01b7394
Fedor Kotov <fedor-keman@yandex.ru>
2023-03-03 01:22:34 +0300

    ver [2.1]

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,18 @@
-let popupFormOpen = document.querySelector(".profile__edit-button");
-let popupForm = document.querySelector(".popup");
-let popupFormClosed = document.querySelector(".popup__close-icon");
-let formElement = document.querySelector(".popup__container");
-let nameInput = document.querySelector(".popup__input_user_name");
-let jobInput = document.querySelector(".popup__input_user_profession");
-let profileName = document.querySelector(".profile__name");
-let profileJob = document.querySelector(".profile__profession");
+let popupFormOpen = document.querySelector('.profile__edit-button');
+let popupForm = document.querySelector('.popup');
+let popupFormClosed = document.querySelector('.popup__close-icon');
+let formElement = document.querySelector('.popup__container');
+let nameInput = document.querySelector('.popup__input_user_name');
+let jobInput = document.querySelector('.popup__input_user_profession');
+let profileName = document.querySelector('.profile__name');
+let profileJob = document.querySelector('.profile__profession');
 
 function popupOpen() {
-  popupForm.classList.add("popup_opened");
+  popupForm.classList.add('popup_opened');
 }
 
 function popupClose() {
-  popupForm.classList.remove("popup_opened");
+  popupForm.classList.remove('popup_opened');
 }
 
 function handleFormSubmit(evt) {
@@ -22,6 +22,6 @@ function handleFormSubmit(evt) {
   popupClose();
 }
 
-formElement.addEventListener("submit", handleFormSubmit);
-popupFormOpen.addEventListener("click", popupOpen);
-popupFormClosed.addEventListener("click", popupClose);
+popupFormOpen.addEventListener('click', popupOpen);
+popupFormClosed.addEventListener('click', popupClose);
+formElement.addEventListener('submit', handleFormSubmit);
