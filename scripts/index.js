936a76b2e653917d18e63e163c7634aecae33367
Fedor Kotov <fedor-keman@yandex.ru>
2023-03-03 01:08:08 +0300

    ver [2.0]

---
+++ scripts/index.js
@@ -0,0 +1,27 @@
+let popupFormOpen = document.querySelector(".profile__edit-button");
+let popupForm = document.querySelector(".popup");
+let popupFormClosed = document.querySelector(".popup__close-icon");
+let formElement = document.querySelector(".popup__container");
+let nameInput = document.querySelector(".popup__input_user_name");
+let jobInput = document.querySelector(".popup__input_user_profession");
+let profileName = document.querySelector(".profile__name");
+let profileJob = document.querySelector(".profile__profession");
+
+function popupOpen() {
+  popupForm.classList.add("popup_opened");
+}
+
+function popupClose() {
+  popupForm.classList.remove("popup_opened");
+}
+
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileJob.textContent = jobInput.value;
+  popupClose();
+}
+
+formElement.addEventListener("submit", handleFormSubmit);
+popupFormOpen.addEventListener("click", popupOpen);
+popupFormClosed.addEventListener("click", popupClose);
